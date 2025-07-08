b62d55ed99918045cc6a41ae8215d37e33b2833e
Ludmily Oliveira <ludmily.oliveira@hotmail.com>
2025-07-08 10:26:27 -0300

    configuração do banco de dados e module

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_farmapi',
+      entities: [], //liga as minhas classes da entity no banco de dados
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
